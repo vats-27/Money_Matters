fc191d83023399f2e3ac8bbfbd37e6e6f59236c6
vats-27 <vshah7530@gmail.com>
2024-09-06 04:12:43 +0530

    header logo

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Hero from './Hero/Hero'
+import Logo from './Logo/Logo'
+import './Home.css'
 
 function Home() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <Hero />
     </div>
-  )
+  );
 }
 
 export default Home
